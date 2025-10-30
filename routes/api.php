2ef37c586f061c3f9d35ab7d9876002a0c36def1
Ahmed elazhary <ahmedelazhary804@gmail.com>
2025-10-30 21:06:10 +0300

    feat: Implement Google authentication with ID token verification and user creation

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Api\Auth\GoogleAuthController;
 
 Route::post('/register', [RegisterController::class, 'register'])->name('register');
 Route::post('/login', [AuthSessionController::class, 'login'])->name('login');
-// Route::post('/auth/google/callback', [GoogleAuthController::class, 'googleLogin'])->name('google.login'); //need more info
+Route::post('/auth/google', [GoogleAuthController::class, 'googleAuth'])->name('google.login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/user', [AuthSessionController::class, 'user'])->name('user');
